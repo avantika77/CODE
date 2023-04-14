77fcfbd30024f1883253b6832b58bb0d1ee518e8
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-04-14 18:52:41 +0530

    Update #1firststep.py

--- #1firststep.py
+++ #1firststep.py
@@ -1,56 +1,274 @@
+
+Skip to content
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+@avantika77
+avantika77 /
+CODE
+Public
+
+Fork your own copy of avantika77/CODE
+
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+
+    Settings
+
+CODE/
+in
+main
+
+1
+
 #literals,vlue types in python
+
+2
+
 print(type(223))
+
+3
+
 print(type(8.7))
+
+4
+
 print(type("hello world"))
 
+5
+
+​
+
+6
+
 print(type("34"))
+
+7
+
 print(type(8.67))
 
+8
+
+​
+
+9
+
 print(type(None))
+
+10
+
 print(type(True))
+
+11
+
 print(type(False))
 
+12
+
+​
+
+13
+
 print(type([]))
+
+14
+
 print(type({}))
 
+15
+
+​
+
+16
+
 #print(type(hello))  #error 'helllo' is not defined
+
+17
+
 print("still runnning")
 
+18
+
+​
+
+19
+
 # floating point limitation
+
+20
+
 print (0.5+7.5) 
+
+21
+
 print(0.1 + 0.2)
 
+22
+
+​
+
+23
+
 #rectangular(numerical opperations)
+
+24
+
 width=45
+
+25
+
 height=54
+
+26
+
 area=width*height
+
+27
+
 print("area=",area)
 
+28
+
+​
+
+29
+
 #width = "23"
+
+30
+
 #height = "17"
+
+31
+
 #area = width * height
+
+32
+
 #print(area)
 
+33
+
+​
+
+34
+
 #add the number
+
+35
+
 a=33
+
+36
+
 b=55
+
+37
+
 c=a+b 
+
+38
+
 print(c)
+
+39
+
+​
+
+40
 
 #string addtion
+
+41
+
 a="23"
+
+42
+
 b="56"
+
+43
+
 c=a+b
+
+44
+
 print(c)
 
+45
+
+​
+
+46
+
 #calculator python
+
+47
+
 r=7
+
+48
+
 import math
+
+49
+
 print("the area is", r * r * math.pi)
+
+50
+
 print("circumference is",2*r*math.pi)
+
+51
+
 a=10
+
+52
+
 b=7
+
+53
+
 print(a+b)
+
+54
+
 print(a-b)
+
+55
+
 print(a*b)
+
+56
+
 print(a/b)
+
+@avantika77
+Commit changes
+Commit summary
+Optional extended description
+Commit directly to the main branch.
+Create a new branch for this commit and start a pull request. Learn more about pull requests.
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
