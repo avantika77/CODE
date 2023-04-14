f0202f1fdbd7b91e0cbf1acc1e03ce1965c9a211
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-04-14 18:48:20 +0530

    Add files via upload

---
+++ #1firststep.py
@@ -0,0 +1,56 @@
+#literals,vlue types in python
+print(type(223))
+print(type(8.7))
+print(type("hello world"))
+
+print(type("34"))
+print(type(8.67))
+
+print(type(None))
+print(type(True))
+print(type(False))
+
+print(type([]))
+print(type({}))
+
+#print(type(hello))  #error 'helllo' is not defined
+print("still runnning")
+
+# floating point limitation
+print (0.5+7.5) 
+print(0.1 + 0.2)
+
+#rectangular(numerical opperations)
+width=45
+height=54
+area=width*height
+print("area=",area)
+
+#width = "23"
+#height = "17"
+#area = width * height
+#print(area)
+
+#add the number
+a=33
+b=55
+c=a+b 
+print(c)
+
+#string addtion
+a="23"
+b="56"
+c=a+b
+print(c)
+
+#calculator python
+r=7
+import math
+print("the area is", r * r * math.pi)
+print("circumference is",2*r*math.pi)
+a=10
+b=7
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
