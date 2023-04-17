d9ad6c64e32a0e20385034994ebf3bcbcf003366
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-04-17 20:28:11 +0530

    Update #2secondstep.py

--- #2secondstep.py
+++ #2secondstep.py
@@ -25,3 +25,45 @@ print("after")
 # if_name_ == "_main_"
 #    main()
     
+val=input("type in a decimal:")
+print(val)
+print(val.isdecimal())
+print(val.isdecimal())
+if val.isdecimal():
+    num=int(val)
+    print(num)
+    
+   #converting string to integer 
+a=23
+print(a)
+print(type(a))
+#<class 'str'>
+
+# converting float to int
+a=2.1
+print(type(a))
+#<class 'float'>
+print(a)
+
+#conditons:if
+def  main():
+    expected_answer = 45
+    inp=input("Please enter the answer:")
+    if inp==expected_answer:
+        print("welcome")
+        main()
+   
+   #condition :if else:   
+# def main():
+#      a = input('First number: ')
+#      n = input('Second number: ')
+# if int(n) == 0:
+#         print("Cannot divide by 0")
+# else:
+#      print("Dividing", a, "by", n)
+#      print(int(a) / int(n))
+# main()
+
+
+
+
