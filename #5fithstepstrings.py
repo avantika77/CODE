741018be8c9fed29c583c8602856e87c7dfc9b2b
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-05-06 08:56:51 +0530

    Add files via upload

---
+++ #5fithstepstrings.py
@@ -0,0 +1,29 @@
+#text inside pair of qoutes single or double
+soup="spices carrot lentil soup"
+print (soup)
+#long line
+text="abc"  "def"
+print (text)
+long_string= "adjg" "dwdd" "efdeq"
+print(long_string)
+#short row
+short_rows = "one" \
+    "two" \
+    "three"
+print(short_rows)
+#long_string
+long_string="first seconf third row "
+print(long_string)
+#triple qoutes string
+text="""first row
+second row
+third row"""
+print(text)
+#string length
+line="hello learner"
+hw=len(line)
+print(hw)
+text="""hello
+learners"""
+print(len(text))
+
