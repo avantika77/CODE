dc43525ee847ae64266ac1b79840ed61636e0f80
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-05-15 17:52:47 +0530

    Update #5fithstepstrings.py

--- #5fithstepstrings.py
+++ #5fithstepstrings.py
@@ -152,7 +152,30 @@ for i in range (23,125):
   print(i,chr(i))
   
 #one string in another string 
+#Write script that gets a character on the command line and prints out the ascii code of it.
+import sys
+if len(sys.argv) != 3:
+ exit(f"Usage: {sys.argv[0]} short-STRING ong-STRING")
+string = sys.argv[1]
+text   = sys.argv[2]
+if string in text:
+  loc = text.index(string)
+  print(string, "can be found in ", text, "at", loc)
+else:
+  print(string, "can NOT be found in ", text)
+  
+#to ASCII CLI
+import sys
+if len(sys.argv) != 2:
+  exit(f"Usage: {sys.argv[0]} CHARACTER")
+print( ord( sys.argv[1]) )
     
+#from ASCII CLI
+import sys
+if len(sys.argv) != 2:
+  exit(f"Usage: {sys.argv[0]} NUMBER")
+  print( chr( int(sys.argv[1]) ) )
+        
     
     
 
