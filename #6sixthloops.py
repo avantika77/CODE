fb1db6ebfc21d021c21fc845fb100b4cff562615
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-05-15 18:04:43 +0530

    Add files via upload

---
+++ #6sixthloops.py
@@ -0,0 +1,6 @@
+#Loops: for-in and while
+#for in - to iterate over a well defined list of values.while - repeate an action till some condition is met. (or stopped being met)
+txt = 'hello world'
+for c in txt:
+    print(c)
+    
