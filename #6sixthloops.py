78acc6910054a874e88d46bc8089bb7bc41ab082
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-05-16 19:33:08 +0530

    Update #6sixthloops.py

--- #6sixthloops.py
+++ #6sixthloops.py
@@ -4,3 +4,12 @@ txt = 'hello world'
 for c in txt:
     print(c)
     
+ #for-in loop on list
+for fruit in ["Apple", "Banana", "Peach", "Orange", "Durian", "Papaya"]:
+   print(fruit)
+
+#for-in loop on
+for i in range(3, 7):
+    print(i)
+
+#Iterable, iterator
