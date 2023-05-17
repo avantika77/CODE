b804870c80fe9e741896271cf8b59db91603b5d2
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-05-17 19:44:44 +0530

    Update #6sixthloops.py

--- #6sixthloops.py
+++ #6sixthloops.py
@@ -37,6 +37,16 @@ for cr in txt:
 print(cr)
 print('DONE')
 
+#while loop
+import random
+total = 0
+while total <= 100:
+ print(total)
+ total += random.randrange(20)
+ print("done")
+
+
+
 
     
  
