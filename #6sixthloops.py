4f5350ca6ced04bbd3f6ccbe16566c802e944f59
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-05-19 18:55:28 +0530

    Update #6sixthloops.py

--- #6sixthloops.py
+++ #6sixthloops.py
@@ -74,6 +74,21 @@ while total < 10000000:
   break
 print("done")
 
+#While True
+import random
+total = 0
+while True:
+   print(total)
+   total += random.randrange(20)
+   if total >= 10000000:
+         break 
+   if total % 17 == 1:
+         break 
+   if total ** 2 % 23 == 7:
+        break
+print("done")
+
+
 
 
 
