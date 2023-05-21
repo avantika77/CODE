92025c9b4c83e72050b1a0b81ddaee120e686968
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-05-21 10:56:05 +0530

    Update #6sixthloops.py

--- #6sixthloops.py
+++ #6sixthloops.py
@@ -109,6 +109,20 @@ while True:
         break
 print("done")
 
+#Print all the locations in a string
+'''text = "The black cat climbed the green tree" 
+start = 0
+while True:
+    loc = text.find("c", start)
+    if loc == -1:
+      break
+      print(loc)
+    start = loc + 1'''
+
+
+
+
+
 
     
  
