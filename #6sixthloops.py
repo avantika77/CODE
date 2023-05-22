c77342ef256626f669d4b5f07d0ac0713d213cbd
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-05-22 20:32:03 +0530

    Update #6sixthloops.py

--- #6sixthloops.py
+++ #6sixthloops.py
@@ -134,6 +134,24 @@ while True:
       break
       print(loc)
     start = loc + 1'''
+#Count unique characters
+import random
+hidden = random.randrange(1, 201)
+while True:
+   user_input = input("Please enter your guess[x]: ")
+   print(user_input)
+   if user_input == 'x':
+         print("Sad to see you leaving early")
+   exit()
+   guess = int(user_input)
+   if guess == hidden:
+         print("Hit!")
+         break
+if guess < hidden:
+         print("Your guess is too low")
+else:
+       print("Your guess is too high")
+
 
 
 
