fa0c47441c7358854599a88ff58d9e37a6e3abd8
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-06-22 17:11:59 +0530

    Add files via upload

---
+++ #f-string.py
@@ -0,0 +1,6 @@
+#f-string 
+name="Dev"
+age=18
+branch ="computer science"
+print("my name is ",name,"I am",age,"year old","from branch", branch)
+print(f"My name is {name}.I am {age} year old.From branch {branch}")#clear amd simple
