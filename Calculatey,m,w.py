c3b13308de2b01b149b8ff8cb4f2bfc6e4f0b956
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-06-23 10:44:40 +0530

    Add files via upload

---
+++ Calculatey,m,w.py
@@ -0,0 +1,16 @@
+#wap how many days,weeks,months we have left if we live until 70 year old
+#i year=265 days,52 weeks , 12 months
+
+age=int (input("enter your age:"))
+years=90-age
+days=years*365
+months=years*12
+weeks=years*52
+
+print(f"you have {days} days,{weeks} weeks,{years} years left")
+
+
+
+
+
+
