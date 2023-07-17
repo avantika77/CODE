4e513c21ebce675e3ecf38770d39d0bf93a66019
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-07-17 12:22:32 +0530

    Update #7seventhformattedprinting.py

--- #7seventhformattedprinting.py
+++ #7seventhformattedprinting.py
@@ -25,5 +25,33 @@ print( "The year is {1} in which the user was born {0}.".format(text,numb))
 #using name
 print("the year is {age} in which {name} was born.".format(name=text, age=numb))
 
+#format column
+data=[
+    ["agnes", 18],
+    ["gloriya",20],
+    ["mert",21878],
+    ["destiny goods",4],
+    ["joe",685854998797],
+]
+for entry in data:
+    print("{} {}".format(entry[0],entry[1]))
+
+print('_' * 16)
+for entry in data:
+    print("{:<8} | {:>7}".format(entry[0],entry[1]))
+    
+#using format -alignment
+txt=" hello agnes"
+print ("'{}'".format(txt))
+print ("'{:12}'".format(txt))
+print ("'{:<12}'".format(txt))
+print ("'{:>12}'".format(txt))
+print ("'{:^12}'".format(txt))
+
+#format string
+name="hello loco"
+print("{:s}".format(name))
+print("{}".format(name))
+
 
 
