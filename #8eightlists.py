9da7cabc9ee597925427fa95ac1cdfa96a562d40
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-07-31 19:42:10 +0530

    Add files via upload

---
+++ #8eightlists.py
@@ -0,0 +1,11 @@
+'''Anything can be a lists
+Comma separated values
+In square brackets
+Can be any value, and a mix of values: Integer, Float, Boolean, None, String, List, Dictionary, …
+But usually they are of the same type:
+Distances of astronomical objects
+Chemical Formulas
+Filenames
+Names of devices
+Objects describing attributes of a network device.
+Actions to do on your data.'''
