f82347c89f81d0049b672117282734f626e6cab8
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-08-08 20:36:38 +0530

    Update #8eightlists.py

--- #8eightlists.py
+++ #8eightlists.py
@@ -45,6 +45,16 @@ print(planets[2:])        # ['Earth', 'Mars', 'Jupiter', 'Saturn']
 print(planets[:3])        # ['Mercury', 'Venus', 'Earth']
 print(planets[:])         # ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn\ ']
 
+#List slice with steps
+''List slice with step: [start:end:step]'''
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+print(letters[::])       # ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+print(letters[::1])      # ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+print(letters[::2])      # ['a', 'c', 'e', 'g', 'i']
+print(letters[1::2])     # ['b', 'd', 'f', 'h', 'j']
+print(letters[2:8:2])    # ['c', 'e', 'g']
+print(letters[1:20:3])   # ['b', 'e', 'h']
+
 
 
 
