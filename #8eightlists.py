aada4fd6f70f3b0a9ee4e3ab1ed821b7326b1799
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-08-15 17:03:22 +0530

    Update #8eightlists.py

--- #8eightlists.py
+++ #8eightlists.py
@@ -72,7 +72,13 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 print(numbers)  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 numbers[1::2] = [0, 0, 0, 0, 0, 0]
 print(numbers)  # [1, 0, 3, 0, 5, 0, 7, 0, 9, 0, 11, 0]
+
 #List assignment and list copy
+x = ['apple', 'bob', 'cat', 'drone']
+y = x
+x[0] = 'qqrq'
+print(x)    # ['qqrq', 'bob', 'cat', 'drone']
+print(y)    # ['qqrq', 'bob', 'cat', 'drone']
 
 
 
