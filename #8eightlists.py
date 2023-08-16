52e5ed95ce08fc73249fc2c4f10341448dcabab4
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-08-16 15:43:53 +0530

    Update #8eightlists.py

--- #8eightlists.py
+++ #8eightlists.py
@@ -79,6 +79,15 @@ y = x
 x[0] = 'qqrq'
 print(x)    # ['qqrq', 'bob', 'cat', 'drone']
 print(y)    # ['qqrq', 'bob', 'cat', 'drone']
+#There is one list in the memory and two pointers to it.
+#If you really want to make a copy the pythonic way is to use the slice syntax.
+#It creates a shallow copy.
+x = ['apple', 'bob', 'cat', 'drone']
+y = x[:]
+x[0] = 'qqrq'
+print(x)    # ['qqrq', 'bob', 'cat', 'drone']
+print(y)    # ['apple', 'bob', 'cat', 'drone']
+
 
 
 
