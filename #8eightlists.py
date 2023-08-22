863e5c6b3571c76fc7675a7fa0c65a57b29befd4
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-08-22 19:13:27 +0530

    Update #8eightlists.py

--- #8eightlists.py
+++ #8eightlists.py
@@ -101,6 +101,10 @@ fields = ['one', 'two and three', 'four', 'five']
 together = ':'.join(fields)
 print(together) # one:two and three:four:five
 mixed = ' -=<> '.join(fields)
+mixed = ' -=<> '.join(fields)
+print(mixed) # one -=<> two and three -=<> four -=<> five
+another = ''.join(fields)
+print(another)  # onetwo and threefourfive
 
 
 
