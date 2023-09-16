24485fa9c784e01f59b6bb19e08123ba2b119f30
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-09-16 22:51:12 +0530

    Update #8eightlists.py

--- #8eightlists.py
+++ #8eightlists.py
@@ -182,4 +182,12 @@ names = ['Joe', 'Kim', 'Jane', 'Bob', 'Kim']
 print(names)                # ['Joe', 'Kim', 'Jane', 'Bob', 'Kim']
 print(names.remove('Kim'))  # None
 print(names)                # ['Joe', 'Jane', 'Bob', 'Kim']
-
+##########################################
+'''Remove first element from a list given by its value.
+Throws an exception if there is no such element in the list.
+------------------------
+print(names.remove('George'))
+# Traceback (most recent call last):
+#   File "examples/lists/remove.py", line 9, in <module>
+#     print(names.remove('George'))  # None
+# ValueError: list.remove(x): x not in list // "error will be like this"
